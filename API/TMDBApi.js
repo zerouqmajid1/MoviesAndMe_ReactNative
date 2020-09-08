81619356811468bceef36bc40d921a0f7914bbda
Your Name <you@example.com>
2020-09-08 16:16:49 +0200

    “project”

---
+++ API/TMDBApi.js
@@ -0,0 +1,21 @@
+
+const API_TOKEN = "5ad3c93a658cf046041747b2a5a7ee6c";
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+
+}
+// API/TMDBApi.js
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+  .then((response) => response.json())
+  .catch((error) => console.error(error));
+}
